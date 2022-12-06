874a31a29db81f9f5915572720ce96c3ff130854
anshverma1612 <115801856+anshverma1612@users.noreply.github.com>
2022-12-06 22:47:43 +0530

    Add files via upload

---
+++ L293 Auto_Z task.ino
@@ -0,0 +1,15 @@
+int in1 = 8;
+int in2 = 9; 
+void setup() {
+  // put your setup code here, to run once:
+ pinMode (8,OUTPUT);
+ pinMode (9,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite (in1,HIGH);
+digitalWrite (in2,LOW);
+
+}
